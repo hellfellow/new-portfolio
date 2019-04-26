17953bfd35571b99e1094ba7705b412c0883176b
hellfellow <49310546+hellfellow@users.noreply.github.com>
2019-04-26 22:27:14 +0300

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,13 @@
+﻿window.onscroll=()=>{
+const navbar=document.querySelector('header')
+if (window.pageYOffset !== 0){navbar.style.boxShadow = '0 0 10px black';}
+else {navbar.style.boxShadow = '';}}
+
+
+$( ".skill" ).click(function() {
+  $( ".skill" ).animate({ "left": "+=50px" }, "slow" );
+});
+
+
+
+
